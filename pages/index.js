81181ee7975d258bcfdee0627f067c638bcc08c9
Wilhelm Alfke <wilhelm.alfke@hotmail.com>
2022-11-16 16:46:26 +0100

    fetch pictures from db.json

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+import styled from "styled-components";
+import useSWR from "swr";
+import { fetcher } from "../helpers/api";
+
 export default function Home() {
-  return <h1>Hello Flamingos! 🦩</h1>;
+  const { data: movies, error } = useSWR("/api/movies", fetcher);
+  console.log(movies);
+  if (!movies) {
+    return <h2> loading..</h2>;
+  }
+  if (error) return <div>failed to load</div>;
+
+  return (
+    <>
+      <h1>series & movies</h1>
+      {movies.map((movie) => {
+        return (
+          <div>
+            <ListItem>
+              <img src={movie.picture} />
+            </ListItem>
+          </div>
+        );
+      })}
+    </>
+  );
 }
+
+const ListItem = styled.li`
+  padding: 0;
+`;
