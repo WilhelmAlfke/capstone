4bf3e47ee145924095f50718043155e3547ffb09
Wilhelm Alfke <wilhelm.alfke@hotmail.com>
2022-11-27 13:55:45 +0100

    adjust button text and hover feat

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -39,7 +39,7 @@ export default function MovieDetailPage({ movies, toggleMovie }) {
               toggleMovie(movieDetail.id);
             }}
           >
-            add to favorites
+            add/remove
           </Button>
         </WrapButton>
 
