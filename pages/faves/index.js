d47f9cfc41ae0d88a9ecb6d90e918c4dddbb2f01
Wilhelm Alfke <wilhelm.alfke@hotmail.com>
2022-12-09 12:58:25 +0100

    test

--- pages/faves/index.js
+++ pages/faves/index.js
@@ -11,7 +11,7 @@ export default function Faves({ movies, toggleFavoriteMovie }) {
       <Head>
         <title>Watch List</title>
         <meta property="og:title" content="Watch List" key="title" />
-        <Link rel="icon" href="/favicon2.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Header headerTitle="watchlist" variant="faves" />
       <main>
